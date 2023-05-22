92b0926282507fce1fe0eac55b5c8bae82314c36
Sobarzo Licandeo <84886339+sobarzolicandeo@users.noreply.github.com>
2023-05-21 20:59:54 -0400

    Update SeleniumCarritoComprasTest.java

--- src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
+++ src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
@@ -12,6 +12,7 @@ import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -22,10 +23,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumCarritoComprasTest {
     private WebDriver driver;
-    
+
+@BeforeClass
+public static void deleteFolder() throws Exception {
+    String imageFileDir = System.getProperty("user.dir") + "/screenshots";
+    FileUtils.deleteDirectory(new File(imageFileDir));
+}
+
 @Before
 public void SetUp() throws Exception{
-    deleteFolder();
     String baseUrl = "http://localhost:8080/CarritoCompras/";
     String chromePath = System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
     System.setProperty("webdriver.chrome.driver", chromePath);
@@ -49,31 +55,49 @@ public void validarNombreProducto() {
         }
     try {
         takeScreenShot("CP-UE02-1_ValidarNombreProducto_"+timestamp()+".png");// Capture screenshot of current state
-    } catch (Exception ex) {
+    } catch (IOException ex) {
+        Logger.getLogger(SeleniumCarritoComprasTest.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
+
+    @Test
+    public void validarDescripcionProducto() {
+    driver.findElement(By.xpath("//a[@href='Controlador?accion=home']")).click();
+    //Obtener texto del producto
+    String textoActualproducto = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/label[1]")).getText();
+    //Variable con valor esperado
+    String textoEsperadoproducto = "producto 1";
+    assertEquals(textoActualproducto, textoEsperadoproducto);
+        // TODO review the generated test code and remove the default call to fail.
+        if(!textoActualproducto.equals(textoEsperadoproducto)) {
+            fail("ERROR: Se esperaba el valor Descripcion producto 1");
+        }
+    try {
+        takeScreenShot("CP-UE04-1_ValidarDescripcionProducto_"+timestamp()+".png");// Capture screenshot of current state
+    } catch (IOException ex) {
         Logger.getLogger(SeleniumCarritoComprasTest.class.getName()).log(Level.SEVERE, null, ex);
     }
     }
 
     @After
     public void tearDown(){
-        driver.quit();
+        if (driver != null){
+            driver.close();
+            driver.quit();
+        }
     }
-
+    
     private void takeScreenShot(String fname) throws IOException {
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String imageFileDir = System.getProperty("user.dir") + "\\screenshots";
-        FileUtils.deleteDirectory(new File(imageFileDir));
-        if (imageFileDir == null)
-            imageFileDir = System.getProperty("java.io.tmpdir");
-        FileUtils.copyFile(scrFile, new File(imageFileDir, fname));
+        // Capture the screenshot
+        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // Specify the destination path for the screenshot file
+        String destinationPath = System.getProperty("user.dir") + "/screenshots";
+        // Save the screenshot to the specified path
+        FileUtils.copyFile(screenshotFile, new File(destinationPath + "/" + fname));
     }
     
     public static String timestamp() {
         return new SimpleDateFormat("MM-dd-yyyy_HH.mm.ss").format(new Date());
     }
     
-    private void deleteFolder() throws Exception {
-        String imageFileDir = System.getProperty("user.dir") + "\\screenshots";
-        FileUtils.deleteDirectory(new File(imageFileDir));
-    }
 }
