ccf0cf1b3ff80a48acfdd90aae1fd885f6f01e99
Diego Sobarzo <sobarzolicandeo@gmail.com>
2023-05-21 21:34:04 -0400

    descripcion producto

--- src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
+++ src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
@@ -64,17 +64,17 @@ public void validarNombreProducto() {
     public void validarDescripcionProducto() {
     driver.findElement(By.xpath("//a[@href='Controlador?accion=home']")).click();
     //Obtener texto del producto
-    String textoActualproducto = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/label[1]")).getText();
+    String textoActualdproducto = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/label[1]")).getText();
     //Variable con valor esperado
-    String textoEsperadoproducto = "producto 1";
-    assertEquals(textoActualproducto, "textoEsperadoproducto");
+    String textoEsperadodproducto = "es un producto";
+    assertEquals(textoActualdproducto, textoEsperadodproducto);
     try {
         takeScreenShot("CP-UE04-1_ValidarDescripcionProducto_"+timestamp()+".png");// Capture screenshot of current state
     } catch (IOException ex) {
         Logger.getLogger(SeleniumCarritoComprasTest.class.getName()).log(Level.SEVERE, null, ex);
     }
     // TODO review the generated test code and remove the default call to fail.
-    if(!textoActualproducto.equals(textoEsperadoproducto)) {
+    if(!textoActualdproducto.equals(textoEsperadodproducto)) {
         fail("ERROR: Se esperaba el valor Descripcion producto 1");
     }
     }
