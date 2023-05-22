d9aa622ecc244cd3faca9a847aa06e27150b5ddb
Diego Sobarzo <sobarzolicandeo@gmail.com>
2023-05-21 21:18:31 -0400

    Refactors

--- src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
+++ src/test/java/seleniumcarritocompras/SeleniumCarritoComprasTest.java
@@ -49,14 +49,14 @@ public void validarNombreProducto() {
     //Variable con valor esperado
     String textoEsperadoproducto = "producto 1";
     assertEquals(textoActualproducto, textoEsperadoproducto);
-        // TODO review the generated test code and remove the default call to fail.
-        if(!textoActualproducto.equals(textoEsperadoproducto)) {
-            fail("ERROR: Se esperaba el valor Descripcion producto 1");
-        }
     try {
         takeScreenShot("CP-UE02-1_ValidarNombreProducto_"+timestamp()+".png");// Capture screenshot of current state
     } catch (IOException ex) {
         Logger.getLogger(SeleniumCarritoComprasTest.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        // TODO review the generated test code and remove the default call to fail.
+    if(!textoActualproducto.equals(textoEsperadoproducto)) {
+        fail("ERROR: Se esperaba el valor Descripcion producto 1");
     }
     }
 
@@ -67,15 +67,15 @@ public void validarNombreProducto() {
     String textoActualproducto = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/label[1]")).getText();
     //Variable con valor esperado
     String textoEsperadoproducto = "producto 1";
-    assertEquals(textoActualproducto, textoEsperadoproducto);
-        // TODO review the generated test code and remove the default call to fail.
-        if(!textoActualproducto.equals(textoEsperadoproducto)) {
-            fail("ERROR: Se esperaba el valor Descripcion producto 1");
-        }
+    assertEquals(textoActualproducto, "textoEsperadoproducto");
     try {
         takeScreenShot("CP-UE04-1_ValidarDescripcionProducto_"+timestamp()+".png");// Capture screenshot of current state
     } catch (IOException ex) {
         Logger.getLogger(SeleniumCarritoComprasTest.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    // TODO review the generated test code and remove the default call to fail.
+    if(!textoActualproducto.equals(textoEsperadoproducto)) {
+        fail("ERROR: Se esperaba el valor Descripcion producto 1");
     }
     }
 
